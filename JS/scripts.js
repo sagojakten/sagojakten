27301fe249b874f26c1ad161a0423a23ce3e6428
cimnex <cimnex@users.noreply.github.com>
2021-05-28 02:51:48 +0200

    basically release 0.1

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,18 +9,91 @@ window.onload = function () {
     });
 };
 
-AFRAME.registerComponent("markerhandler", {
+AFRAME.registerComponent("registerevents", {
   init: function () {
-    const animatedMarker = document.querySelector("#animated-marker");
-    const aEntity = document.querySelector("#findus");
+    const marker = this.el;
 
-    // every click, we make our model grow in size :)
-    animatedMarker.addEventListener("click", function (ev, target) {
-      const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-      if (aEntity && intersectedElement === aEntity) {
-        const scale = aEntity.getAttribute("scale");
-        Object.keys(scale).forEach((key) => (scale[key] = scale[key] + 1));
-        aEntity.setAttribute("scale", scale);
+    marker.addEventListener("markerFound", () => {
+      var markerId = marker.id;
+      console.log("Marker Found: ", markerId);
+
+      if (markerId == "pettson-marker") {
+        var pettsonEntity = document.querySelector("#pettson_character");
+        var pettsonSound = document.querySelector("#pettson_sound");
+        pettsonEntity.addEventListener("click", function () {
+          if (pettsonSound.paused) {
+            pettsonSound.play();
+          } else {
+            pettsonSound.pause();
+          }
+        });
+      }
+      if (markerId == "lassemaja-marker") {
+        var lassemajaEntity = document.querySelector("#lassemaja_characters");
+        var lassemajaSound = document.querySelector("#lassemaja_sound");
+        lassemajaEntity.addEventListener("click", function () {
+          if (lassemajaSound.paused) {
+            lassemajaSound.play();
+          } else {
+            lassemajaSound.pause();
+          }
+        });
+      }
+      if (markerId == "roda_masken-marker") {
+        var roda_maskenEntity = document.querySelector(
+          "#roda_masken_character"
+        );
+        var roda_maskenSound = document.querySelector("#roda_masken_sound");
+        roda_maskenEntity.addEventListener("click", function () {
+          if (roda_maskenSound.paused) {
+            roda_maskenSound.play();
+          } else {
+            roda_maskenSound.pause();
+          }
+        });
+      }
+      if (markerId == "rumpnisse-marker") {
+        var rumpnisseEntity = document.querySelector("#rumpnisse_clickable");
+        var rumpnisseSound = document.querySelector("#rumpnisse_sound");
+        rumpnisseEntity.addEventListener("click", function () {
+          if (rumpnisseSound.paused) {
+            rumpnisseSound.play();
+          } else {
+            rumpnisseSound.pause();
+          }
+        });
+      }
+      if (markerId == "karlsson-marker") {
+        var karlssonEntity = document.querySelector("#karlsson_character");
+        var karlssonSound = document.querySelector("#karlsson_sound");
+        karlssonEntity.addEventListener("click", function () {
+          if (karlssonSound.paused) {
+            karlssonSound.play();
+          } else {
+            karlssonSound.pause();
+          }
+        });
+      }
+    });
+
+    marker.addEventListener("markerLost", () => {
+      var markerId = marker.id;
+      console.log("Marker Lost: ", markerId);
+
+      if (markerId == "pettson-marker") {
+        pettsonSound.pause();
+      }
+      if (markerId == "lassemaja-marker") {
+        lassemajaSound.pause();
+      }
+      if (markerId == "roda_masken-marker") {
+        roda_maskenSound.pause();
+      }
+      if (markerId == "rumpnisse-marker") {
+        rumpnisseSound.pause();
+      }
+      if (markerId == "karlsson-marker") {
+        karlssonSound.pause();
       }
     });
   },
